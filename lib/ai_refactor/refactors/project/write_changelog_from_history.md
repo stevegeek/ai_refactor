32012f60c95a94bcc22d52782a8a4b2c18dad632
Stephen Ierodiaconou <stevegeek@gmail.com>
2023-07-26 09:37:05 +0300

    Write changelog from git history refactor

---
+++ lib/ai_refactor/refactors/project/write_changelog_from_history.md
@@ -0,0 +1,35 @@
+You are a senior Ruby software developer. You are diligent about writing clear changelogs for your users.
+
+You have a git history of changes to your project. You want to write a changelog from this history.
+
+The format of the changelog you will create entries for is from keepachangelog.com version 1.0.0.
+
+Here is an example of a set of changelog entries:
+
+```
+## [0.2.0] - 2015-10-06
+
+### Changed
+
+- Remove exclusionary mentions of "open source" since this project can
+  benefit both "open" and "closed" source projects equally.
+
+## [0.1.0] - 2015-10-06
+
+### Added
+
+- Answer "Should you ever rewrite a change log?".
+
+### Changed
+
+- Improve argument against commit logs.
+- Start following [SemVer](https://semver.org) properly.
+
+### Fixed
+
+- Fix typos in recent CHANGELOG entries.
+```
+
+If the history contains any mention of changes of version (such as "Bump version to 1.0.0"), then you should use that version number in a new changelog entry.
+
+Now write changelog entries from the following git history.
---
+++ lib/ai_refactor/refactors/project/write_changelog_from_history.rb
@@ -0,0 +1,50 @@
+# frozen_string_literal: true
+
+module AIRefactor
+  module Refactors
+    module Project
+      class WriteChangelogFromHistory < BaseRefactor
+        def self.description
+          "Write changelog entries from the git history"
+        end
+
+        def self.takes_input_files?
+          false
+        end
+
+        def run
+          logger.verbose "Creating changelog entries for project from #{options[:git_commit_count] || 3} commits..."
+          logger.verbose "Write output to #{output_file_path}..." if output_file_path
+
+          self.input_content = `git log -#{options[:git_commit_count] || 3} --pretty=format:"%s"`.split("\n").map { |line| "- #{line}" }.join("\n")
+          logger.debug "\nInput messages: \n#{input_content}\n\n"
+          # begin
+          output_content = process!(strip_ticks: false)
+          # rescue => e
+          #   logger.error "Failed to process: #{e.message}"
+          #   return false
+          # end
+
+          return false unless output_content
+
+          output_file_path ? true : output_content
+        end
+
+        def default_output_path
+          nil
+        end
+
+        def self.command_line_options
+          [
+            {
+              key: :git_commit_count,
+              long: "--commits N",
+              type: Integer,
+              help: "The number of commits to analyse when creating the changelog entries (defaults to 3)"
+            }
+          ]
+        end
+      end
+    end
+  end
+end
